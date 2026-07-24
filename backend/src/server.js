986c2cc37382b68206f84f031e13b46ad9831c8b
Amena Akhter Chowdhury <amenaakhterchy@gmail.com>
2026-07-24 02:56:55 +0200

    this is full devellopment project("Fix axios + router + CORS")

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,7 +13,20 @@ import { app, server } from "./lib/socket.js";
 const PORT = ENV.PORT||3000;
 
 app.use(express.json({limit:"5mb"}));// this is the middleware we are calling so that we can get access to the fields the user send - req.body
-app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+//app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  ENV.CLIENT_URL, // production frontend
+];
+
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  })
+);
+
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
