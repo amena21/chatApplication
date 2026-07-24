3ccad5911dfd8e8832649c115696f80b18b17e93
Amena Akhter Chowdhury <amenaakhterchy@gmail.com>
2026-07-24 04:01:16 +0200

    cookie fix for  production

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -4,18 +4,19 @@ import { ENV } from "./env.js";
 export const generateToken = (userId,res) => {
     const token = jwt.sign({ userId }, ENV.JWT_SECRET, {
         expiresIn: "7d",
-    });
+    }); 
 
-
+    //production
     res.cookie("jwt", token, {
-    maxAge: 7 * 24 * 60 * 60 * 1000,
-    httpOnly: true,
-    secure: true,        // REQUIRED for Vercel + Render (HTTPS)
-    sameSite: "none",    // REQUIRED for cross‑site cookies
-    path: "/",           // ensures cookie is sent on all routes
-    });
+        maxAge: 7*24*60*60*1000, //MS
+        httpOnly: true, // prevent XSS attacks: cross-site scripting
+        secure: true, // REQUIRED for Vercel + Render (HTTPS)
+        sameSite: "none",// REQUIRED for cross-site cookies
+        path: "/",// ensures cookie is sent everywhere
 
-    return token;
+    });
+    
+    return token; 
 };
 
 //http://localhost
