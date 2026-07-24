b7f97bad35dbab1e5d27e2a71f00b56c66baa03a
Amena Akhter Chowdhury <amenaakhterchy@gmail.com>
2026-07-24 03:43:52 +0200

    Fix cookie settings for production

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -6,14 +6,6 @@ export const generateToken = (userId,res) => {
         expiresIn: "7d",
     });
 
-  /* localhost  
-  res.cookie("jwt", token, {
-        maxAge: 7*24*60*60*1000, //MS
-        httpOnly: true, // prevent XSS attacks: cross-site scripting
-        sameSite: "strict", // CSRF attacks
-        secure: ENV.NODE_ENV === "development" ? false : true,
-
-    }); */
 
     res.cookie("jwt", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000,
